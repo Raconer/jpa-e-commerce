569c1ecef4fd4cecb4702cd70d4a68ca8bb94558
Dongho Kim <cha11enge@hanmail.net>
2023-06-10 16:18:21 +0900

    [mod] Service Test Setting 변경 (#5)

--- src/test/java/com/jpa/commerce/service/CategoryServiceTest.java
+++ src/test/java/com/jpa/commerce/service/CategoryServiceTest.java
@@ -1,12 +1,13 @@
 package com.jpa.commerce.service;
 
 import com.jpa.commerce.entity.category.CategoryEntity;
-import com.jpa.commerce.repository.category.CategoryRepository;
+import com.jpa.commerce.service.category.CategoryService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
@@ -14,14 +15,16 @@ import java.util.List;
 @DisplayName("Category Service Test")
 @DataJpaTest
 @ActiveProfiles("test")
+@Import(CategoryService.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CategoryServiceTest {
 
     @Autowired
-    private CategoryRepository categoryRepository;
+    private CategoryService categoryService;
+
     @Test
     public void getListTest(){
-        List<CategoryEntity> categoryList = this.categoryRepository.findAll();
+        List<CategoryEntity> categoryList = this.categoryService.getList();
         System.out.println(categoryList.toString());
     }
 
