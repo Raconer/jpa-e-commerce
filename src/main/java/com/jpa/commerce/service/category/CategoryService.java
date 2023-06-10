81dd287ec8fdb4e3334adb3f02e635ed416dcd4e
Dongho Kim <cha11enge@hanmail.net>
2023-06-10 12:51:20 +0900

    [add] 기본 테스트구조 셋팅 (#3)

--- src/main/java/com/jpa/commerce/service/category/CategoryService.java
+++ src/main/java/com/jpa/commerce/service/category/CategoryService.java
@@ -1,7 +1,21 @@
 package com.jpa.commerce.service.category;
 
+import com.jpa.commerce.entity.category.CategoryEntity;
+import com.jpa.commerce.repository.category.CategoryRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
+    private CategoryRepository categoryRepository;
+    public CategoryService( CategoryRepository categoryRepository){
+        this.categoryRepository = categoryRepository;
+    }
+
+    // READ
+    public List<CategoryEntity> getList(){
+        return this.categoryRepository.findAll();
+    }
+
 }
