f4589f932408e15adb0ed454e7e4a73742fc73d2
Raconer <cha11enge@hanmail.net>
2023-06-13 22:35:12 +0900

    [mod] Repository 추가

---
+++ src/main/java/com/jpa/commerce/repository/transit/TransitRepository.java
@@ -0,0 +1,7 @@
+package com.jpa.commerce.repository.transit;
+
+import com.jpa.commerce.entity.transit.TransitEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransitRepository extends JpaRepository<TransitEntity, Long> {
+}
